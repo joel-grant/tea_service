b6124fe11b073fa94e237d962bb5706c9a06520b
Joel Grant <joelmgrant@me.com>
2022-04-19 21:33:21 -0600

    Tests: Setup model validation tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Subscription do
     it { should belong_to(:tea) }
     it { should belong_to(:customer) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+  end
 end
