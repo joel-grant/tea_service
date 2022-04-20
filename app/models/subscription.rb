3d9a75cf7e69b6c1c53f9cfc4af80e26f392f317
Joel Grant <joelmgrant@me.com>
2022-04-19 22:01:05 -0600

    Setup enums for status and frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,6 @@ class Subscription < ApplicationRecord
   validates_presence_of :title, :price, :status, :frequency, presence: true
   belongs_to :tea
   belongs_to :customer
+  enum status: [ :active, :inactive ]
+  enum frequency: [ :weekly, :biweekly, :monthly ]
 end
