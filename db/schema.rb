837a224e0cdf8a13c6e552f52dcacf8d17701d19
Joel Grant <joelmgrant@me.com>
2022-04-19 21:22:18 -0600

    Create subscription migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_20_025030) do
+ActiveRecord::Schema.define(version: 2022_04_20_032139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2022_04_20_025030) do
     t.string "address"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.integer "status"
+    t.integer "frequency"
+    t.bigint "tea_id"
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -29,4 +40,6 @@ ActiveRecord::Schema.define(version: 2022_04_20_025030) do
     t.string "brew_time"
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
