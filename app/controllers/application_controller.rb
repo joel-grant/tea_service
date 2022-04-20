b7b7dd339d94225ec756ce1e2795df7f917bdcac
Joel Grant <joelmgrant@me.com>
2022-04-20 10:47:25 -0600

    Feat: Allow a user to filter by active or inactive subscriptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::API
-  def index
-    customer = Customer.find(params[:customer_id])
-    subs = customer.subscriptions
-    render json: SubscriptionSerializer.new(subs)
-  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,12 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email, :address, presence: true
   has_many :subscriptions
   has_many :teas, through: :subscriptions
+
+  def get_active_subscriptions
+    subscriptions.where(status: :active)
+  end
+
+  def get_cancelled_subscriptions
+    subscriptions.where(status: :cancelled)
+  end
 end
