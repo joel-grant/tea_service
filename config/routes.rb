1b48f7e77f8bd1e11d7516c3ce16622d0c758ca1
Joel Grant <joelmgrant@me.com>
2022-04-22 07:39:43 -0600

    Update routes with customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :subscriptions, only: [:index, :create, :destroy]
+      resources :customers, only: [] do
+        resources :subscriptions, shallow: true, only: [:index, :create, :destroy]
+      end
     end
   end
 end
